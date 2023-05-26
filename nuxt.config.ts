f77e98d4a771dc7cbe9c725f4e69b7328db44e5c
Basil Suter <git@nadar.io>
2023-05-26 15:01:16 +0000

    rename

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["bootstrap/dist/css/bootstrap.min.css"], // add
-  modules: [["@flyodev/nitrocms-nuxt", {
+  modules: [["@flyo/nitro-nuxt", {
     apiToken: process.env.NODE_ENV !== 'production' ? 'DEV_TOKEN' : 'PRODUCTION_TOKEN'
   }]],
 });
